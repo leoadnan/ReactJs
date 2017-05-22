17057a83c315dbf02440585bd522eb21d71248c5
Adnan Ahmed <adahmed@nisum.com>
2017-05-21 22:43:15 -0700

    Todo App Refactored

--- random-examples/src/App.js
+++ random-examples/src/App.js
@@ -1,84 +1,15 @@
 import React, {Component} from 'react';
-//import Jumbotron from './components/Jumbotron';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import TodoApp from './components/TodoApp';
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            text: '',
-            isEdit: 0,
-            todos: [
-                {
-                    id: 1,
-                    text: 'Todo-1'
-                }, 
-                {
-                    id: 2,
-                    text: 'Todo-2'
-                },
-                {
-                    id: 3,
-                    text: 'Todo-3'
-                }
-            ]
-        };
+        this.state = {};
     }
     render() {
-        return (
-            <div>
-                <TodoForm 
-                    {...this.state}
-                    addTodo={this.handleAddTodo.bind(this)} 
-                    changeText={this.handleChangeText.bind(this)}
-                    onTodoUpdate={this.handleOnTodoUpdate.bind(this)} />
-
-                <TodoList 
-                   {...this.state}
-                    deleteTodo={this.handleDeleteTodo.bind(this)} 
-                    editTodo={this.handleEditTodo.bind(this)} />
-            </div>
+        return (            
+            <TodoApp />
         );
-    }
-
-    handleAddTodo(text) {
-        var newTodo = {
-            id: this.state.todos.length + 1,
-            text: text
-        }
-
-        this.setState(
-            {
-                todos: this.state.todos.concat(newTodo)
-            }
-        )
-    }
-    handleDeleteTodo(todo) {
-        var todos = this.state.todos;
-        var index = todos.findIndex(t => t.id===todo.id)
-        todos.splice(index,1);
-        this.setState({todos:todos})
-    } 
-    handleEditTodo(todo) {
-        this.setState({
-            text: todo.text,
-            isEdit: todo.id
-        });
-    }
-
-    handleChangeText(value){
-        this.setState({
-            text: value
-        })
-    }
-
-    handleOnTodoUpdate(todo){
-        var todos = this.state.todos;
-        var index = todos.findIndex(t => t.id===todo.id)
-        todos.splice(index,1);
-        todos.push(todo);
-        this.setState({todos:todos})
     }
 }
 
